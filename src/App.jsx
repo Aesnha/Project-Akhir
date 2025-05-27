e3b5f7764ca55987e8349fe92572b716764d0d47
Aesnha <aisyahn14568494@gmail.com>
2025-05-27 10:54:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-blue-500">
+          Kelompok 3 auto win
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
