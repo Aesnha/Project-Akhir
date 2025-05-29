f8da10b67d3d5563483928da57b8c56aab31e6d9
nebulaa01 <abillakmal019@gmail.com>
2025-05-30 01:48:49 +0700

    new main

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <div className="w-full min-h-screen bg-[#192338]">
+      <div className="flex items-center justify-center h-full text-[#D9E1F1]">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
