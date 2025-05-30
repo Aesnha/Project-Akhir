2709297a6aff20575212234716a5ec45eef0de72
nebulaa01 <abillakmal019@gmail.com>
2025-05-30 17:57:27 +0700

    revisi main

--- src/assets/HomeSection/HeroSection.jsx
+++ src/assets/HomeSection/HeroSection.jsx
@@ -2,30 +2,29 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-   <section className='w-full min-h-screen bg-[#192338]'>
-        <div className='flex items-center justify-center h-screen'>
-           <div className='flex flex-col md:flex-row rounded-ss-full items-center h-1/2 bg-[#1E2E4F] shadow-lg shadow-[#31487A] '>
-            <h1 className='p-4 font-montserrat text-2xl md:text-3xl lg:text-6xl text-center text-[#D9E1F1]  text-shadow-lg text-shadow-[#8FB3E2]'>
+    <section className="w-full min-h-screen font-[montserrat] bg-[#192338] pt-16 md:pt-0">
+      <div className="flex flex-col md:flex-row items-center justify-center h-screen px-4">
+        <div className="flex-1 flex flex-col justify-center items-center md:items-start rounded-ss-full h-auto bg-[#1E2E4F] shadow-lg shadow-[#31487A] p-4 sm:p-6 md:p-12 mb-6 md:mb-0">
+          <h1 className="font-bold mb-4 text-2xl mx-6 sm:text-3xl md:text-4xl lg:text-6xl text-center md:text-left text-white drop-shadow">
             COOL.In
-            </h1>
-            <div className='w-1/2'>
-           
-            <p className=' p-6 text-xl font-montserrat md:text-1xl text-[#D9E1F1]'>
-          Hidup sudah cukup panas jadi biarkan minumanmu tetap dingin. Dapatkan tumbler COOL.IN
-            </p>
-           
-            </div>
-           </div>
-           <div className='w-1/2 flex md:flex-col p-10 shadow-lg items-center justify-center shadow-[#8FB3E2] gap-3'> 
-            <img src="CWT.png" alt="" className='rounded-full'
-             width={300}
-                height={300} 
-            />
-             <button className=' md:text-1xl sm:flex-col text-center p-3 rounded-full py-3 border-white  bg-amber-50 hover:bg-[#31487A] shadow-grey-700'>
-                 <a href="/product">Order Now!</a>
-            </button>
-           </div>
+          </h1>
+          <p className="px-2 sm:px-0 md:px-6 text-base sm:text-lg md:text-xl font-semibold text-[#D9E1F1] text-center md:text-left">
+            Hidup sudah cukup panas jadi biarkan minumanmu tetap dingin. Dapatkan tumbler COOL.In
+          </p>
         </div>
+        <div className="flex-1 w-full flex flex-col items-center justify-center gap-3 p-6">
+          <img
+            src="CWT.png"
+            alt=""
+            className="w-auto h-40 sm:w-auto sm:h-56 md:w-auto md:h-90 object-cover"
+            width={300}
+            height={300}
+          />
+          <button className="text-base md:text-lg text-center p-3 rounded-full py-3 border-white bg-amber-50 hover:bg-[#31487A] transition-colors">
+            <a href="/product">Order Now!</a>
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
--- src/assets/HomeSection/TestimonialSection.jsx
+++ src/assets/HomeSection/TestimonialSection.jsx
@@ -1,61 +1,77 @@
 import React from 'react'
 
 export default function TestimonialSection() {
-  return ( 
-     <section id="testimonials" className="py-20 bg-[#192338]">
-        <div className="container mx-auto px-4 bg-[#31487A] rounded-se-full">
-          <div className="text-center mb-16 mx-auto rounded-full items-center justify-center ">
-            <h2 className="text-3xl font-montserrat text-center md:text-4xl font-bold text-[#D9E1F1] mb-4 p-3">What They Say</h2>
-            <div className="w-20 h-1   mx-auto"></div>  
+  return (
+    <section id="testimonials" className="py-20 bg-[#192338]">
+      <div className="container font-[montserrat] mx-auto px-4 bg-[#31487A] rounded-se-full">
+        <div className="text-center mb-16 mx-auto rounded-full items-center justify-center ">
+          <h2 className="text-3xl text-center md:text-4xl font-bold text-[#D9E1F1] ">
+            What They Say
+          </h2>
+          <div className="w-20 h-1   mx-auto"></div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4 ">
+          {/* Testimonial 1 */}
+
+          <div className="bg-gray-50 p-8 rounded-lg shadow-lg shadow-[#D9E1F1] rounded-r-md  ">
+            <div className="flex items-center justify-center mb-4 ">
+              <div className="p-3 text-center">
+                <h4 className="text-xl font-bold text-[#192338]]">
+                  Rendi Hermawan
+                </h4>
+                <p className="text-medium text-[#192338]">
+                  Atlet Lari
+                </p>
+              </div>
+            </div>
+            <p className="text-[#192338] text-sm italic">
+              "Sebagai Atlet Lari, tumbler COOL.IN adalah penyelamat! air tetap
+              dingin meski di ruangan outdoor."
+            </p>
           </div>
-        
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4 ">
-            {/* Testimonial 1 */}
-           
-            <div className="bg-gray-50 p-8 rounded-lg shadow-lg shadow-[#D9E1F1] rounded-r-md  ">
-              <div className="flex items-center justify-center mb-4 ">
-                <div className='p-3 text-center'>
-                  <h4 className="text-lg font-semibold text-[#1E2E4F]">Rendi Hermawan</h4>
-                  <p className="text-sm text-[#8FB3E2]">Atlet Lari</p>
-                </div>
-              </div>
-              <p className="text-[#8FB3E2] italic">
-                "Sebagai Atlet Lari, tumbler COOL.IN adalah penyelamat! air tetap dingin meski di ruangan outdoor."
-              </p>
-            
-            </div>
 
-            {/* Testimonial 2 */}
-           <div className="bg-gray-50 p-8 rounded-lg shadow-lg shadow-[#D9E1F1]  ">
-              <div className="flex items-center justify-center mb-4 ">
-                <div className='p-3 text-center'>
-                  <h4 className="text-lg font-semibold text-[#1E2E4F]">Floren Lestari</h4>
-                  <p className="text-sm text-[#8FB3E2]">Perawat</p>
-                </div>
-              </div>
-              <p className="text-[#8FB3E2] italic">
-                "Bekerja di tempat super sibuk, tubuh harus terhidrasi dengan baik. Meski harus minum di sela - sela menangani pasien. Air yang dibawa dari pagi tetap dingin, rasanya menenangkan minumnya waktu tenggorokan kering."
-              </p>
-             
-            </div>
-
-            {/* Testimonial 3 */}
+          {/* Testimonial 2 */}
           <div className="bg-gray-50 p-8 rounded-lg shadow-lg shadow-[#D9E1F1]  ">
-              <div className="flex items-center justify-center mb-4 ">
-                <div className='p-3 text-center'>
-                  <h4 className="text-lg font-semibold text-[#1E2E4F]">Suherman</h4>
-                  <p className="text-sm text-[#8FB3E2]">Petani</p>
-                </div>
+            <div className="flex items-center justify-center mb-4 ">
+              <div className="p-3 text-center">
+                <h4 className="text-xl font-bold text-[#192338]">
+                  Floren Lestari
+                </h4>
+                <p className="text-medium text-[#192338]">
+                  Perawat
+                </p>
               </div>
-              <p className="text-[#8FB3E2] italic">
-                "Di ladang, cuaca bisa sangat panas, dan minum air dingin adalah hal yang sangat dinanti. Tumblr ini luar biasa. Sampai tengah hari, masih dingin rasanya. Sekarang, badan saya rasanya lebih segar dan tidak cepat lemas."
-              </p>
-              
             </div>
+            <p className="text-[#192338] text-sm italic">
+              "Bekerja di tempat super sibuk, tubuh harus terhidrasi dengan
+              baik. Meski harus minum di sela - sela menangani pasien. Air yang
+              dibawa dari pagi tetap dingin, rasanya menenangkan minumnya waktu
+              tenggorokan kering."
+            </p>
+          </div>
 
-            
+          {/* Testimonial 3 */}
+          <div className="bg-gray-50 p-8 rounded-lg shadow-lg shadow-[#D9E1F1]  ">
+            <div className="flex items-center justify-center ">
+              <div className="p-3 text-center">
+                <h4 className="text-xl font-bold text-[#192338]">
+                  Suherman
+                </h4>
+                <p className="text-medium text-[#192338]">
+                  Petani
+                </p>
+              </div>
+            </div>
+            <p className="text-[#192338] text-sm italic">
+              "Di ladang, cuaca bisa sangat panas, dan minum air dingin adalah
+              hal yang sangat dinanti. Tumblr ini luar biasa. Sampai tengah
+              hari, masih dingin rasanya. Sekarang, badan saya rasanya lebih
+              segar dan tidak cepat lemas."
+            </p>
           </div>
         </div>
-      </section>
-  )
+      </div>
+    </section>
+  );
 }
